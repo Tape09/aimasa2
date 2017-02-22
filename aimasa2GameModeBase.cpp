fc29876d7198baa34192bb8d6df46eae65c63716
Tape09 <tapeworm09@hotmail.com>
2017-02-22 14:09:22 +0100

    first

---
+++ aimasa2GameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "aimasa2.h"
+#include "aimasa2GameModeBase.h"
+
+
+
+
---
+++ aimasa2GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "aimasa2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIMASA2_API Aaimasa2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
