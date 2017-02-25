3657530237993a819bc15488204095e9226d214b
Mike H <tapeworm09@hotmail.com>
2017-02-25 13:40:28 +0100

    fixed pi

--- aimasa2.Build.cs
+++ aimasa2.Build.cs
@@ -8,7 +8,7 @@ public class aimasa2 : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent", "Json", "JsonUtilities" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
